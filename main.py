5aed62de737a9e5ec451b633c795593cd1f86a0a
bburns <arachidonic.acid@gmail.com>
2010-11-01 14:38:06 -0500

    got add book form working

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
+from google.appengine.ext.db import djangoforms
+
 from xml.dom import minidom
 from xmlExport import xmlExport
 from xmlImport import xmlImportString
-from models import Student
+#from models import Student
+from models import *
     
 """
 ASN1
@@ -73,11 +76,56 @@ class ClearData(webapp.RequestHandler):
         db.delete(query)
         self.redirect("/")
 
+
+class BookForm(djangoforms.ModelForm):
+    class Meta:
+        model = Book
+        # exclude = ['isbn']
+
+class BookData(webapp.RequestHandler):
+    def get(self):
+       self.response.out.write('<html><body>'
+                                '<form method="post" '
+                                'action="/book">'
+                                '<table>')
+       # This generates our shopping list form and writes it in the response
+       self.response.out.write(BookForm())
+       self.response.out.write('</table>'
+                                '<input type="submit">'
+                                '</form></body></html>')        
+    def post(self):
+        self.response.out.write("eriwrueiru")
+        data = BookForm(data=self.request.POST)
+        if data.is_valid():
+            self.response.out.write("valid data")
+            # Save the data, and redirect to the view page
+            book = data.save() #(commit=False)
+            #book.added_by = users.get_current_user()
+            book.put()
+            self.response.out.write("hello")
+            self.redirect('/book')
+            # self.response.out.write("Successfully Added:" + book.title)
+        else:
+            # Reprint the form
+            self.response.out.write('<html><body>'
+                                    '<form method="post" '
+                                    'action="/book">'
+                                    '<table>jlijlijlij')
+            self.response.out.write(data)
+            self.response.out.write('</table>'
+                                    '<input type="submit">'
+                                    '</form></body></html>')
+
+
+
+
 _URLS = (
      ('/', MainPage),
      ('/export',ExportData),
      ('/import',ImportData),
-     ('/dbclear',ClearData))
+     ('/book', BookData),
+     ('/dbclear',ClearData)
+     )
 
 def main():
     "Run the webapp"
