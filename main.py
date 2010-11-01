82b5aa3fa1deb249137ddda523d8e8d76928e7b5
bburns <arachidonic.acid@gmail.com>
2010-11-01 15:41:47 -0500

    got book/list, book/add, book/edit working, with no frills

--- main.py
+++ main.py
@@ -77,22 +77,46 @@ class ClearData(webapp.RequestHandler):
         self.redirect("/")
 
 
+
 class BookForm(djangoforms.ModelForm):
     class Meta:
         model = Book
         # exclude = ['isbn']
 
-class BookData(webapp.RequestHandler):
+class BookList(webapp.RequestHandler):
     def get(self):
-       self.response.out.write('<html><body>'
+        """
+        Return the book page (import.html).
+        """
+        books = Book.all()
+        
+        template_values = {'books': books}
+        directory = os.path.dirname(__file__)
+        path = os.path.join(directory, 'booklist.html')
+        self.response.out.write(template.render(path, template_values, True))
+
+        #self.response.out.write('<html><body>')
+        #self.response.out.write('<table>')
+
+        #for book in books:
+        #    self.response.out.write(book.title + " " )
+        #self.response.out.write('</table>')
+        #self.response.out.write('</body></html>')
+
+
+
+class BookAdd(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("add")
+        self.response.out.write('<html><body>'
                                 '<form method="post" '
-                                'action="/book">'
+                                'action="/book/add">'
                                 '<table>')
-       # This generates our shopping list form and writes it in the response
-       self.response.out.write(BookForm())
-       self.response.out.write('</table>'
+        # This generates our shopping list form and writes it in the response
+        self.response.out.write(BookForm())
+        self.response.out.write('</table>'
                                 '<input type="submit">'
-                                '</form></body></html>')        
+                                '</form></body></html>')
     def post(self):
         self.response.out.write("eriwrueiru")
         data = BookForm(data=self.request.POST)
@@ -103,18 +127,87 @@ class BookData(webapp.RequestHandler):
             #book.added_by = users.get_current_user()
             book.put()
             self.response.out.write("hello")
-            self.redirect('/book')
+            self.response.out.write("   id is '%s'" % book.key().id())
+            self.redirect('/book/list')
             # self.response.out.write("Successfully Added:" + book.title)
         else:
             # Reprint the form
             self.response.out.write('<html><body>'
                                     '<form method="post" '
-                                    'action="/book">'
-                                    '<table>jlijlijlij')
+                                    'action="/book/add">'
+                                    '<table>')
             self.response.out.write(data)
             self.response.out.write('</table>'
                                     '<input type="submit">'
                                     '</form></body></html>')
+
+
+class BookEdit(webapp.RequestHandler):
+    def get(self):
+        id = int(self.request.get('id'))
+        self.response.out.write("id is: '%s'" % id)
+        key = db.Key.from_path('Book', id)
+        self.response.out.write("key is: '%s'" % key)
+        
+        #book = Book.get(key)
+        book = Book.get_by_id(id)
+        self.response.out.write("book is '%s'" % book.title)
+        self.response.out.write('<html><body>'
+                                '<form method="POST" '
+                                'action="/book/edit">'
+                                '<table>')
+        self.response.out.write(BookForm(instance=book))
+        self.response.out.write('</table>'
+                                '<input type="hidden" name="_id" value="%s">'
+                                '<input type="submit">'
+                                '</form></body></html>' % id)
+
+    def post(self):
+      id = int(self.request.get('_id'))
+      book = Book.get(db.Key.from_path('Book', id))
+      data = BookForm(data=self.request.POST, instance=book)
+      if data.is_valid():
+          # Save the data, and redirect to the view page
+          entity = data.save(commit=False)
+          # entity.added_by = users.get_current_user()
+          entity.put()
+          self.redirect('/book/list')
+      else:
+          # Reprint the form
+          self.response.out.write('<html><body>'
+                                  '<form method="POST" '
+                                  'action="/book/edit">'
+                                  '<table>')
+          self.response.out.write(data)
+          self.response.out.write('</table>'
+                                  '<input type="hidden" name="_id" value="%s">'
+                                  '<input type="submit">'
+                                  '</form></body></html>' % id)
+
+    # def post(self):
+    #     data = BookForm(data=self.request.POST)
+    #     if data.is_valid():
+    #         self.response.out.write("valid data")
+    #         # Save the data, and redirect to the view page
+    #         book = data.save(commit=False)
+    #         #book.added_by = users.get_current_user()
+    #         id = book.key().id()
+    #         key = db.Key.from_path('Book', id)
+    #         book.key
+    #         book.put()
+    #         self.response.out.write("   id is '%s'" % book.key().id())
+    #         self.redirect('/book/list')
+    #         # self.response.out.write("Successfully Added:" + book.title)
+    #     else:
+    #         # Reprint the form
+    #         self.response.out.write('<html><body>'
+    #                                 '<form method="post" '
+    #                                 'action="/book/edit">'
+    #                                 '<table>')
+    #         self.response.out.write(data)
+    #         self.response.out.write('</table>'
+    #                                 '<input type="submit">'
+    #                                 '</form></body></html>')
 
 
 
@@ -123,7 +216,9 @@ _URLS = (
      ('/', MainPage),
      ('/export',ExportData),
      ('/import',ImportData),
-     ('/book', BookData),
+     ('/book/list', BookList),
+     ('/book/add', BookAdd),
+     ('/book/edit', BookEdit),
      ('/dbclear',ClearData)
      )
 
